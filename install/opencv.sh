603712613dabf727b1b8d9df83a3db0afc21d1da
kanokkorn <kanokorn@outlook.jp>
2020-03-02 02:31:07 +0700

    fix typo, add few flags and lib

--- install/opencv.sh
+++ install/opencv.sh
@@ -11,20 +11,24 @@ apt install -y git \
                libavformat-dev \
                libswscale-dev \
                libxvidcore-dev \
-               libx264-dev
+               libx264-dev \
+               libatlas-base-dev
 cd ~ && git clone https://github.com/opencv/opencv.git && git clone https://github.com/opencv/opencv_contrib.git
 mkdir ~/opencv_build && cd ~/opencv_build
 cmake ../opencv -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D INSTALL_PYTHON_EXAMPLE=OFF \
+      -D INSTALL_PYTHON_EXAMPLE=ON \
       -D OPENCV_GENERATE_PKGCONFIG=ON \
       -D ENABLE_VFPV3=ON \
       -D BUILD_TESTS=OFF \
       -D OPENCV_ENABLE_NONFREE=ON \
       -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
       -D BUILD_EXAMPLES=OFF \
-      -D SOFTFP=ON
-      -D BUILD_opencv_python2=0
-      -D BUILD_opencv_python3=1
+      -D SOFTFP=ON \
+      -D BUILD_opencv_python2=0 \
+      -D BUILD_opencv_python3=1 \
+      -D OPENCV_PYTHON3_VERSION=ON \
+      
 make -j4
-sudo make install
+sudo make install && sudo python3 ~/opencv_build/python_loader/setup.py install
+pip3 install numpy --user
