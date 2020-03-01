d62bfaedde845e0b894ed3c59eb9ef632c7a0214
kanokkorn <kanokorn@outlook.jp>
2020-03-02 02:41:04 +0700

    add 'sudo' in case of permission denied

--- install/opencv.sh
+++ install/opencv.sh
@@ -1,18 +1,18 @@
-apt install -y git \
-               python3-pip \
-               build-essential \
-               cmake \
-               unzip \
-               pkg-config \
-               libjpeg-dev \
-               libpng-dev \
-               libtiff-dev \
-               libavcodec-dev \
-               libavformat-dev \
-               libswscale-dev \
-               libxvidcore-dev \
-               libx264-dev \
-               libatlas-base-dev
+sudo apt install -y git \
+                  python3-pip \
+                  build-essential \
+                  cmake \
+                  unzip \
+                  pkg-config \
+                  libjpeg-dev \
+                  libpng-dev \
+                  libtiff-dev \
+                  libavcodec-dev \
+                  libavformat-dev \
+                  libswscale-dev \
+                  libxvidcore-dev \
+                  libx264-dev \
+                  libatlas-base-dev
 cd ~ && git clone https://github.com/opencv/opencv.git && git clone https://github.com/opencv/opencv_contrib.git
 mkdir ~/opencv_build && cd ~/opencv_build
 cmake ../opencv -D CMAKE_BUILD_TYPE=RELEASE \
@@ -28,7 +28,7 @@ cmake ../opencv -D CMAKE_BUILD_TYPE=RELEASE \
       -D BUILD_opencv_python2=0 \
       -D BUILD_opencv_python3=1 \
       -D OPENCV_PYTHON3_VERSION=ON \
-      
+
 make -j4
 sudo make install && sudo python3 ~/opencv_build/python_loader/setup.py install
 pip3 install numpy --user
