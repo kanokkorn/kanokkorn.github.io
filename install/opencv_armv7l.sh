9503f59dbba3258d83b1618df905e31a12368b06
kanokkorn <kanokorn@outlook.jp>
2020-05-21 15:18:15 +0700

    remove newline

--- install/opencv_armv7l.sh
+++ install/opencv_armv7l.sh
@@ -6,17 +6,17 @@ sudo apt install -y git binutils-arm-linux-gnueabihf python3-dev python3-pip bui
 sudo pip3 install numpy --user
 cd ~ && git clone https://github.com/opencv/opencv.git && git clone https://github.com/opencv/opencv_contrib.git
 mkdir ~/opencv_build && cd ~/opencv_build
-cmake ~/opencv -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D INSTALL_PYTHON_EXAMPLE=ON \
-      -D ENABLE_VFPV3=ON \
-      -D BUILD_TESTS=ON \
-      -D OPENCV_ENABLE_NONFREE=ON \
-      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-      -D BUILD_EXAMPLES=ON \
-      -D OPENCV_PYTHON3_VERSION=ON \
-      -D WITH_LIBV4L=OFF \
-      -D WITH_GTK=ON \
+cmake ~/opencv -D CMAKE_BUILD_TYPE=RELEASE
+      -D CMAKE_INSTALL_PREFIX=/usr/local 
+      -D INSTALL_PYTHON_EXAMPLE=ON 
+      -D ENABLE_VFPV3=ON 
+      -D BUILD_TESTS=ON 
+      -D OPENCV_ENABLE_NONFREE=ON 
+      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules 
+      -D BUILD_EXAMPLES=ON 
+      -D OPENCV_PYTHON3_VERSION=ON 
+      -D WITH_LIBV4L=OFF 
+      -D WITH_GTK=ON 
       -D WITH_V4L=ON
 make -j4
 sudo make install && sudo python3 ~/opencv_build/python_loader/setup.py develop
