e2b2aaf4d306fb003f156ee039ce42eb0d826afc
kanokkorn <kanokorn@outlook.jp>
2020-06-13 20:50:00 +0700

    add pygubu

--- install/numpy_latest.sh
+++ install/numpy_latest.sh
@@ -1,3 +1,3 @@
 sudo apt install python3 python3-pip git cmake 
-sudo pip3 install cython imutils
+sudo pip3 install cython imutils pygubu
 cd ~ && git clone https://github.com/numpy/numpy.git && sudo pip3 install -e ~/numpy 
